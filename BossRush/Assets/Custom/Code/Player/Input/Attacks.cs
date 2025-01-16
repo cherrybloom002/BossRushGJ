df83647089f63567f14464b05e517b940c25168e
Andrei1207 <andre.cio1203@gmail.com>
2025-01-16 22:22:24 +0100

    Boss attack added, to fix
    To fix when the player stays inside the attack range and the boss has to continiously attack him

--- BossRush/Assets/Custom/Code/Player/Input/Attacks.cs
+++ BossRush/Assets/Custom/Code/Player/Input/Attacks.cs
@@ -5,22 +5,14 @@ public class Attacks : MonoBehaviour
 {
     public GameObject bullet;
     [SerializeField]
-    public Transform attackPoint;
-    GameObject pos;
-    Rigidbody2D rb;
-    Collider2D collider;
+    GameObject attackPoint;
     [SerializeField]
     float attackRange = 5.5f;
     public LayerMask enemyLayers;
-    private float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        pos = GameObject.FindGameObjectWithTag("BulletPoint");
-        attackPoint = pos.GetComponent<Transform>();
-        rb = GetComponent<Rigidbody2D>();
-        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -30,19 +22,18 @@ public class Attacks : MonoBehaviour
     }
     public void shoot(int damage)
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRange, enemyLayers);
 
         foreach (Collider2D hit in hitEnemies) 
         {
             hit.GetComponent<EnemyScript>().TakeDamage(damage);
         }
-        
     }
 
     private void OnDrawGizmos()
     {
         if(attackPoint == null) {  return; }
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        Gizmos.DrawWireSphere(attackPoint.transform.position, attackRange);
     }
 
 }
--- BossRush/Assets/EnemyScript.cs
+++ BossRush/Assets/EnemyScript.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +6,16 @@ public class EnemyScript : MonoBehaviour
 {
     [SerializeField]
     Slider HealthBar;
+    [SerializeField]
+    GameObject attackPoint;
+    [SerializeField]
+    LayerMask Player;
+    [SerializeField]
+    float attackRange = 2.5f;
+    [SerializeField]
+    Animator animator;
+    [SerializeField]
+    Collider2D PlayerCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,16 +25,53 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRange, Player);
+
+        foreach (Collider2D hit in hitPlayer)
+        {
+            //hit.GetComponent<PlayerScript>().TakeDamage(10);
+            animator.SetBool("atk", true);
+        }
     }
 
     public void TakeDamage(int damage)
     {
         HealthBar.value -= damage;
 
-        if(HealthBar.value <= 0 )
+        if (HealthBar.value <= 0)
         {
             Destroy(gameObject);
         }
     }
+
+    public void shoot(Collider2D hit)
+    {
+        //Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRange, Player);
+
+        //foreach (Collider2D hit in hitEnemies)
+        //{
+            hit.GetComponent<PlayerScript>().TakeDamage(10);
+            //animator.SetBool("atk", true);
+        //}
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (attackPoint == null) { return; }
+        Gizmos.DrawWireSphere(attackPoint.transform.position, attackRange);
+    }
+
+    //public void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    if(collision.collider == PlayerCollider)
+    //    {
+    //        animator.SetBool("atk", true);
+    //    }
+    //}
+
+    public void EndAtk()
+    {
+        animator.SetBool("atk", false);
+    }
+
 }
