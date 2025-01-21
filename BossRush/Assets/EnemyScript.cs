e27061f33c240a2fb9dc9f1ccaa790a144a30c55
Andrei1207 <andre.cio1203@gmail.com>
2025-01-21 15:04:22 +0100

    Added animations for Underground atk, still to finish the logic
    Needs to remain up for the head

--- BossRush/Assets/EnemyScript.cs
+++ BossRush/Assets/EnemyScript.cs
@@ -16,6 +16,12 @@ public class EnemyScript : MonoBehaviour
     Animator animator;
     [SerializeField]
     Collider2D PlayerCollider;
+    [SerializeField]
+    Collider2D AttackCollider;
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    Collider2D bodyCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,13 +31,7 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRange, Player);
 
-        foreach (Collider2D hit in hitPlayer)
-        {
-            //hit.GetComponent<PlayerScript>().TakeDamage(10);
-            animator.SetBool("atk", true);
-        }
     }
 
     public void TakeDamage(int damage)
@@ -44,34 +44,71 @@ public class EnemyScript : MonoBehaviour
         }
     }
 
-    public void shoot(Collider2D hit)
+    public void shoot()
     {
-        //Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRange, Player);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRange, Player);
 
-        //foreach (Collider2D hit in hitEnemies)
-        //{
+        foreach (Collider2D hit in hitEnemies)
+        {
             hit.GetComponent<PlayerScript>().TakeDamage(10);
-            //animator.SetBool("atk", true);
-        //}
+        }
     }
+    public void underAtk()
+    {
+        //attackPoint.transform.position = new Vector3(player.transform.position.x, attackPoint.transform.position.y, attackPoint.transform.position.z);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRange, Player);
 
+        foreach (Collider2D hit in hitEnemies)
+        {
+            hit.GetComponent<PlayerScript>().TakeDamage(10);
+        }
+    }
     private void OnDrawGizmos()
     {
         if (attackPoint == null) { return; }
         Gizmos.DrawWireSphere(attackPoint.transform.position, attackRange);
     }
 
-    //public void OnCollisionStay2D(Collision2D collision)
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider == PlayerCollider)
+        {
+            if (HealthBar.value > 50)
+                animator.SetBool("atk", true);
+            else if (HealthBar.value < 51)
+                animator.SetBool("Uatk", true);
+        }
+    }
+
+    //public void Underground()
     //{
-    //    if(collision.collider == PlayerCollider)
-    //    {
-    //        animator.SetBool("atk", true);
-    //    }
+        
+    //    transform.position -= new Vector3(0f, 7f, 0f);
+    //    attackPoint.transform.position = new Vector3(0f, 4f, 0f);
     //}
 
     public void EndAtk()
     {
         animator.SetBool("atk", false);
     }
-
+    public void EndUAtk()
+    {
+        animator.SetBool("Uatk", false);
+    }
+    public void Eyet()
+    {
+        animator.SetBool("Eye", true);
+    }
+    public void Eyef()
+    {
+        animator.SetBool("Eye", false);
+    }
+    public void Sungexit()
+    {
+        animator.SetBool("Uexit", true);
+    }
+    public void Eungexit()
+    {
+        animator.SetBool("Uexit", false);
+    }
 }
