3839d7f3efa254b2343265fa7a71f8e1222853ab
cherrybloom002 <161952780+cherrybloom002@users.noreply.github.com>
2025-01-24 22:35:47 +0100

    Added new boss health bar

--- BossRush/Assets/Custom/Code/Player/Input/Attacks.cs
+++ BossRush/Assets/Custom/Code/Player/Input/Attacks.cs
@@ -13,6 +13,7 @@ public class Attacks : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
     }
 
     // Update is called once per frame
